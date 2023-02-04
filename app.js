68a7b486562755700ffdbfab03ee8e5533f6c952
Frimpong Boateng <executivekfb27@gmail.com>
2023-02-04 16:37:39 +0000

    add javascript functionality to the submit button

---
+++ app.js
@@ -0,0 +1,25 @@
+// SELECTORS
+const submitBtn = document.querySelector(".btn");
+const inputEmail = document.getElementById("email");
+const errorParagraph = document.querySelector(".error-paragraph");
+const errorIcon = document.querySelector(".error-icon");
+
+const emailPattern =  "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/";
+
+// function to validate email
+function validateEmail(e) {
+    e.preventDefault();
+    console.log("I have been clicked");
+    console.log(inputEmail.value);
+    let  emailInput = inputEmail.value;
+
+    if(!emailInput.match(emailPattern) || emailInput === "") {
+        errorParagraph.style.display = "block";
+        errorIcon.style.display = "block";
+    }
+
+}
+
+
+//EVENT LISTENERS
+submitBtn.addEventListener("click", validateEmail);
